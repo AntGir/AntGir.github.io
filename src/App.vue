eb4f5d8e6951e02ee6e080309b1a3abd2612426d
AntGir <57183917+AntGir@users.noreply.github.com>
2025-06-04 19:18:10 +0200

    test 35

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <div class="title">Antoine Gires</div>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/git-logo.svg" width="200" height="200" />
+    <div class="wrapper">
+      <HelloWorld msg="Welcome to my Git-pages" />
+    </div>
+  </header>
+  <div class="main-grid">
+    <main><TheWelcome /></main>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  grid-column: 1/3;
+  grid-row: 1;
+  font-size: 300%;
+  text-align: center;
+  margin-bottom: 50px;
+}
+header {
+  grid-column: 1;
+  grid-row: 2;
+  line-height: 1.5;
+}
+.main-grid {
+  grid-column: 2;
+  grid-row: 2;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+
+const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <a class="img" href="https://github.com/AntGir" target="_blank" rel="noopener">
+        <img
+          alt="github-logo"
+          class="logo"
+          src="../assets/github-mark-white.svg"
+          width="30"
+          height="30"
+        />
+      </a>
+    </template>
+    <template #heading
+      ><a href="https://github.com/AntGir" target="_blank" rel="noopener">My git-hub</a></template
+    >
+    Hi there! Feel free to explore my projects and connect with me!
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <a
+        class="img"
+        href="https://www.linkedin.com/in/antoine-gires-23571a122/"
+        target="_blank"
+        rel="noopener"
+      >
+        <img
+          alt="github-logo"
+          class="logo"
+          src="../assets/linkedin-logo.svg"
+          width="30"
+          height="30"
+        />
+      </a>
+    </template>
+    <template #heading
+      ><a href="https://www.linkedin.com/in/antoine-gires-23571a122/" target="_blank" rel="noopener"
+        >LinkedIn</a
+      ></template
+    >
+    Actuellement a la recherche pour une alternance en Concepteur Développeur 3D VR
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <a
+        class="img"
+        href="https://drive.google.com/drive/folders/1HdrRmcGvsnev47FSvI6nrmfYHTSAR2QD?usp=sharing"
+        target="_blank"
+        rel="noopener"
+      >
+        <img
+          alt="github-logo"
+          class="logo"
+          src="../assets/Google-Drive-icon-logo.svg"
+          width="30"
+          height="30"
+        />
+      </a>
+    </template>
+    <template #heading
+      ><a
+        href="https://drive.google.com/drive/folders/1HdrRmcGvsnev47FSvI6nrmfYHTSAR2QD?usp=sharing"
+        target="_blank"
+        rel="noopener"
+        >Informations Formation Simplon</a
+      >
+    </template>
+    Dans ce drive vous allez retrouver la planning de la formation ainsi que la plaquette de
+    celle-ci.
+  </WelcomeItem>
+</template>
+<style scoped>
+.img {
+  display: flex;
+  align-items: center;
+}
+</style>
